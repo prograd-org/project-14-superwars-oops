d2cab844e3e2fa484f9f4064e904248292aad765
author-skj <suryakamaljain0010@gmail.com>
2022-02-13 20:31:48 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -26,6 +26,12 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
+        this.id = id;
+        this.name = name;
+        this.strength = this.getRandomStrength();
+        this.image = "images/super-" + (id + 1) + ".png";
+        this.type = type;
+        console.log(id + " " + name + " " + type + " " + this.image + " " + this.strength);
 
     }
 
@@ -39,7 +45,18 @@ class Player {
         // Accumulate HTML template
         // Type your code here
 
-    
+        
+        let player = document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id', this.id);
+        let image = document.createElement('img');
+        image.setAttribute('src', this.image);
+        let name = document.createElement('div');
+        name.textContent = this.name;
+        let strength = document.createElement('div');
+        strength.textContent = this.strength;
+        strength.className = 'strength';
+        player.append(image, name, strength);
         return player;
     }
 }
@@ -50,6 +67,11 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
+    this.players = players.map((player, i) => {
+        let type = (i % 2 == 0) ? 'hero' : 'villain';
+        // console.log(i+" "+player+" "+type);
+        return new Player(i, player, type);
+    });
 
     }
 
